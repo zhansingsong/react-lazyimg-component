6b5f28fcb9184f37ab0f7d48521ae184f5a900b1
zhansingsong <zhansingsong@gmail.com>
2021-01-07 19:52:16 +0800

    remove demos

--- demos/src/utils/getRImg.js
+++
@@ -1,9 +0,0 @@
-const context = require.context('../img/rimgs', false, /\.jpg|jpeg|png|gif$/);
-const keys = context.keys();
-const len = keys.length;
-export default (keyss)=>{
-  // let index = Math.floor(Math.random()*len);
-  // let key = keys[index];
-  let key = keys.pop();
-  return context(key);
-}
--- demos/src/utils/index.js
+++
@@ -1,2 +0,0 @@
-export {default as getImg} from './getImg.js';
-export {default as getRImg} from './getRImg.js';
